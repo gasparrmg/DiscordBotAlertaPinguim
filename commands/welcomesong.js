bf178074bfb33f00272df3fdfdcea13388106282
gasparrmg <rafael.gaspar97@gmail.com>
2021-03-26 17:55:53 +0000

    riczao now has now more welcome songs

--- commands/welcomesong.js
+++ commands/welcomesong.js
@@ -3,13 +3,18 @@ const ytdl = require('ytdl-core');
 
 const ytSearch = require('yt-search');
 
+var welcomeSongs = [
+    './audio/welcome_song.mp3',
+    './audio/welcome_song2.mp3'
+];
+
 module.exports = {
     name: 'welcomesong',
     description: 'Welcome song',
     async execute(voiceChannel) {
 
         await voiceChannel.join().then(connection => {
-            const dispatcher = connection.play('./audio/welcome_song.mp3');
+            const dispatcher = connection.play(welcomeSongs[Math.floor(Math.random() * welcomeSongs.length)]);
 
             dispatcher.on('finish', () => {
                 voiceChannel.leave();
