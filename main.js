84b6005080f062eff1c29214c82c327483cf777f
gasparrmg <rafael.gaspar97@gmail.com>
2021-03-26 18:10:49 +0000

    bug fix

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ client.on('message', message => {
 
 client.on('voiceStateUpdate', (oldState, newState) => {
     // check for bot
-    if (!newState.member.user.bot && newState.channel != null && newState.connection != null) {
+    if (!newState.member.user.bot && newState.channel != null) {
         client.commands.get('welcomesong').execute(newState.channel);
     }
 });
